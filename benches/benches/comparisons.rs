8c806b5fe01f04b7dc7038be4cd14b6082c250b1
John-John Tedro <udoprog@tedro.se>
2022-01-06 03:21:06 +0100

    More benches

---
+++ benches/benches/comparisons.rs
@@ -0,0 +1,174 @@
+use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
+use rand::{Rng, RngCore};
+use rowan::{GreenNodeBuilder, SyntaxNode};
+use syntree::{Tree, TreeBuilder, TreeBuilderError};
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(u16)]
+enum SyntaxKind {
+    STRING,
+    ENTRY,
+    WHITESPACE,
+    ROOT,
+}
+
+use SyntaxKind::*;
+
+impl From<SyntaxKind> for rowan::SyntaxKind {
+    fn from(kind: SyntaxKind) -> Self {
+        Self(kind as u16)
+    }
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+enum Lang {}
+
+impl rowan::Language for Lang {
+    type Kind = SyntaxKind;
+
+    fn kind_from_raw(raw: rowan::SyntaxKind) -> Self::Kind {
+        assert!(raw.0 <= ROOT as u16);
+        unsafe { std::mem::transmute::<u16, SyntaxKind>(raw.0) }
+    }
+
+    fn kind_to_raw(kind: Self::Kind) -> rowan::SyntaxKind {
+        kind.into()
+    }
+}
+
+fn syntree_build(strings: &[Box<str>], count: usize) -> Result<Tree<SyntaxKind>, TreeBuilderError> {
+    let mut builder = TreeBuilder::new();
+
+    let c = builder.checkpoint();
+
+    for s in strings.iter().cycle().take(count) {
+        builder.token(STRING, s.len());
+    }
+
+    builder.close_at(c, ROOT)?;
+    builder.build()
+}
+
+fn rowan_build(strings: &[Box<str>], count: usize) -> SyntaxNode<Lang> {
+    let mut builder = GreenNodeBuilder::new();
+
+    let c = builder.checkpoint();
+
+    for s in strings.iter().cycle().take(count) {
+        builder.token(STRING.into(), s);
+    }
+
+    builder.start_node_at(c, ROOT.into());
+    builder.finish_node();
+    SyntaxNode::new_root(builder.finish())
+}
+
+fn rowan_tree(n: usize, strings: &[Box<str>]) -> SyntaxNode<Lang> {
+    let mut builder = GreenNodeBuilder::new();
+
+    let c = builder.checkpoint();
+
+    for (_, s) in (0..n).zip(strings.iter().cycle()) {
+        builder.start_node(ENTRY.into());
+        builder.token(STRING.into(), &**s);
+        builder.finish_node();
+        builder.token(WHITESPACE.into(), " ");
+    }
+
+    builder.start_node_at(c, ROOT.into());
+    builder.finish_node();
+    SyntaxNode::new_root(builder.finish())
+}
+
+fn syntree_tree(n: usize, strings: &[Box<str>]) -> Result<Tree<SyntaxKind>, TreeBuilderError> {
+    let mut builder = TreeBuilder::new();
+
+    let c = builder.checkpoint();
+
+    for (_, s) in (0..n).zip(strings.iter().cycle()) {
+        builder.open(ENTRY);
+        builder.token(STRING.into(), s.len());
+        builder.close()?;
+        builder.token(WHITESPACE.into(), 1);
+    }
+
+    builder.close_at(c, ROOT)?;
+    builder.build()
+}
+
+fn setup(c: &mut Criterion) {
+    let strings = generate_random(100, 5, 20);
+
+    let sizes = [1024, 2048, 4096, 8192, 16384];
+
+    {
+        let mut group = c.benchmark_group("building");
+
+        for size in sizes {
+            group.bench_with_input(BenchmarkId::new("syntree", size), &size, |b, size| {
+                b.iter(|| syntree_build(&strings, *size).expect("failed to build tree"))
+            });
+
+            group.bench_with_input(BenchmarkId::new("rowan", size), &size, |b, size| {
+                b.iter(|| rowan_build(&strings, *size))
+            });
+        }
+    }
+
+    {
+        let mut group = c.benchmark_group("children_full");
+
+        for size in sizes {
+            group.bench_with_input(BenchmarkId::new("syntree", size), &size, |b, size| {
+                let syntree = syntree_tree(*size, &strings).unwrap();
+                let root = syntree.first().unwrap();
+                b.iter(|| root.children().count())
+            });
+
+            group.bench_with_input(BenchmarkId::new("rowan", size), &size, |b, size| {
+                let rowan = rowan_tree(*size, &strings);
+                b.iter(|| rowan.children_with_tokens().count())
+            });
+        }
+    }
+
+    {
+        let mut group = c.benchmark_group("children_nodes_only");
+
+        for size in sizes {
+            group.bench_with_input(BenchmarkId::new("syntree", size), &size, |b, size| {
+                let syntree = syntree_tree(*size, &strings).unwrap();
+                let root = syntree.first().unwrap();
+                b.iter(|| root.children().without_tokens().count())
+            });
+
+            group.bench_with_input(BenchmarkId::new("rowan", size), &size, |b, size| {
+                let rowan = rowan_tree(*size, &strings);
+                b.iter(|| rowan.children().count())
+            });
+        }
+    }
+}
+
+criterion_group!(benches, setup);
+criterion_main!(benches);
+
+fn generate_random(count: usize, min: usize, max: usize) -> Vec<Box<str>> {
+    let mut rng = rand::thread_rng();
+    let mut s = String::with_capacity(16);
+
+    let mut output = Vec::with_capacity(count);
+
+    for _ in 0..count {
+        let target = (rng.gen::<usize>() % (max - min)) + min;
+        assert!(target >= min && target < max, "target was {}", target);
+
+        while s.len() < target {
+            s.extend(char::from_u32(rng.next_u32()).filter(|c| !c.is_whitespace()));
+        }
+
+        output.push(s.as_str().into());
+    }
+
+    output
+}
