3a0b209ecfc95fb4aac221e11ca78ffda0e8249c
John-John Tedro <udoprog@tedro.se>
2023-01-14 01:50:37 +0100

    Release 0.13.2

--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@
 //! Add `syntree` to your crate:
 //!
 //! ```toml
-//! syntree = "0.13.1"
+//! syntree = "0.13.2"
 //! ```
 //!
 //! If you want a complete sample for how `syntree` can be used for parsing, see
