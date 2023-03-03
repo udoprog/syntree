0d182c1bc2d5760ee85b7f9b5d63042a31568e9a
John-John Tedro <udoprog@tedro.se>
2023-03-03 08:21:10 +0100

    Make pointer types public and documented

--- src/pointer.rs
+++ src/pointer.rs
@@ -8,13 +8,40 @@ mod sealed {
 }
 
 /// A pointer type that is derived from the pointer [Width].
-#[doc(hidden)]
 pub trait Pointer: Sized + Copy + hash::Hash + Eq + fmt::Debug + self::sealed::Sealed {
     #[doc(hidden)]
     unsafe fn new_unchecked(value: usize) -> Self;
-    #[doc(hidden)]
+
+    /// Safe constructor for the pointer.
+    ///
+    /// Returns [`None`] if the value provided is out of bounds for the pointer type.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use syntree::pointer::{Pointer, PointerU32};
+    ///
+    /// let v = PointerU32::new(0).ok_or("out of bounds")?;
+    /// assert_eq!(v.get(), 0);
+    /// let v = PointerU32::new(42).ok_or("out of bounds")?;
+    /// assert_eq!(v.get(), 42);
+    /// # Ok::<_, Box<dyn std::error::Error>>(())
+    /// ```
     fn new(value: usize) -> Option<Self>;
-    #[doc(hidden)]
+
+    /// Get the index of a pointer.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use syntree::pointer::{Pointer, PointerU32};
+    ///
+    /// let v = PointerU32::new(0).ok_or("out of bounds")?;
+    /// assert_eq!(v.get(), 0);
+    /// let v = PointerU32::new(42).ok_or("out of bounds")?;
+    /// assert_eq!(v.get(), 42);
+    /// # Ok::<_, Box<dyn std::error::Error>>(())
+    /// ```
     fn get(self) -> usize;
 }
 
@@ -48,77 +75,125 @@ pub trait Width: self::sealed::Sealed {
 }
 
 macro_rules! implement {
-    ($ty:ident, $non_zero:ident) => {
+    ($ty:ident, $non_zero:ident, $e:ident) => {
         impl Width for $ty {
             const EMPTY: Self = 0;
-            type Pointer = self::$ty::Pointer;
+            type Pointer = $e;
         }
 
+        use core::num::$non_zero;
+
         impl self::sealed::Sealed for $ty {}
-        impl self::sealed::Sealed for self::$ty::Pointer {}
+        impl self::sealed::Sealed for $e {}
 
-        mod $ty {
-            use core::fmt;
-            use core::mem::size_of;
-            use core::num::$non_zero;
+        #[doc = concat!(" [`Pointer`] implementation for `", stringify!($ty), "`.")]
+        #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+        #[repr(transparent)]
+        pub struct $e($non_zero);
 
-            #[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-            #[repr(transparent)]
-            pub struct Pointer($non_zero);
-
-            impl crate::pointer::Pointer for Pointer {
-                #[inline]
-                unsafe fn new_unchecked(value: usize) -> Self {
-                    let value = value as $ty;
-                    Self($non_zero::new_unchecked(value ^ <$ty>::MAX))
-                }
-
-                #[inline]
-                fn new(value: usize) -> Option<Self> {
-                    let value = if size_of::<usize>() <= size_of::<$ty>() {
-                        value as $ty
-                    } else {
-                        if value > <$ty>::MAX as usize {
-                            return None;
-                        }
-
-                        value as $ty
-                    };
-
-                    match $non_zero::new((value as $ty) ^ <$ty>::MAX) {
-                        None => None,
-                        Some(value) => Some(Self(value)),
-                    }
-                }
-
-                #[inline]
-                fn get(self) -> usize {
-                    (self.0.get() ^ <$ty>::MAX) as usize
-                }
+        impl $e {
+            #[doc = concat!(" Safe constructor for the pointer.")]
+            ///
+            #[doc = concat!(" Returns [`None`] if the value provided is out of bounds for the pointer type.")]
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use syntree::pointer::Pointer;
+            #[doc = concat!("use syntree::pointer::", stringify!($e), ";")]
+            ///
+            #[doc = concat!(" let v = ", stringify!($e), "::new(0).ok_or(\"out of bounds\")?;")]
+            /// assert_eq!(v.get(), 0);
+            #[doc = concat!(" let v = ", stringify!($e), "::new(42).ok_or(\"out of bounds\")?;")]
+            /// assert_eq!(v.get(), 42);
+            /// # Ok::<_, Box<dyn std::error::Error>>(())
+            /// ```
+            #[inline]
+            pub fn new(value: usize) -> Option<Self> {
+                let value = <$ty>::try_from(value).ok()?;
+                $non_zero::new(value.wrapping_add(1)).map(Self)
             }
 
-            impl Default for Pointer {
-                #[inline]
-                fn default() -> Self {
-                    // SAFETY: we know that max is a legal value and that it
-                    // corresponds to zero.
-                    unsafe { Self($non_zero::new_unchecked(<$ty>::MAX)) }
-                }
+            #[doc = concat!(" Get the index of a pointer.")]
+            ///
+            /// # Examples
+            ///
+            /// ```
+            /// use syntree::pointer::Pointer;
+            #[doc = concat!("use syntree::pointer::", stringify!($e), ";")]
+            ///
+            #[doc = concat!(" let v = ", stringify!($e), "::new(0).ok_or(\"out of bounds\")?;")]
+            /// assert_eq!(v.get(), 0);
+            #[doc = concat!(" let v = ", stringify!($e), "::new(42).ok_or(\"out of bounds\")?;")]
+            /// assert_eq!(v.get(), 42);
+            /// # Ok::<_, Box<dyn std::error::Error>>(())
+            /// ```
+            #[inline]
+            pub fn get(self) -> usize {
+                self.0.get().wrapping_sub(1) as usize
+            }
+        }
+
+        #[doc = concat!(" Pointer used for `", stringify!($ty), "` type.")]
+        ///
+        /// ```
+        /// use syntree::pointer::Pointer;
+        #[doc = concat!("use syntree::pointer::", stringify!($e), ";")]
+        ///
+        #[doc = concat!(" let v = ", stringify!($e), "::new(0).ok_or(\"out of bounds\")?;")]
+        /// assert_eq!(v.get(), 0);
+        #[doc = concat!(" let v = ", stringify!($e), "::new(42).ok_or(\"out of bounds\")?;")]
+        /// assert_eq!(v.get(), 42);
+        /// # Ok::<_, Box<dyn std::error::Error>>(())
+        /// ```
+        impl crate::pointer::Pointer for $e {
+            #[inline]
+            unsafe fn new_unchecked(value: usize) -> Self {
+                let value = value as $ty;
+                Self($non_zero::new_unchecked(value.wrapping_add(1)))
             }
 
-            impl fmt::Debug for Pointer {
-                #[inline]
-                fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-                    (self.0.get() ^ <$ty>::MAX).fmt(f)
-                }
+            #[inline]
+            fn new(value: usize) -> Option<Self> {
+                $e::new(value)
+            }
+
+            #[inline]
+            fn get(self) -> usize {
+                $e::get(self)
+            }
+        }
+
+        /// Construct a default value.
+        ///
+        /// ```
+        /// use syntree::pointer::Pointer;
+        #[doc = concat!("use syntree::pointer::", stringify!($e), ";")]
+        ///
+        #[doc = concat!(" let v = ", stringify!($e), "::default();")]
+        /// assert_eq!(v.get(), 0);
+        /// ```
+        impl Default for $e {
+            #[inline]
+            fn default() -> Self {
+                // SAFETY: we know that 1 is a legal value and that it
+                // corresponds to zero.
+                unsafe { Self($non_zero::new_unchecked(1)) }
+            }
+        }
+
+        impl fmt::Debug for $e {
+            #[inline]
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                self.get().fmt(f)
             }
         }
     };
 }
 
-implement!(usize, NonZeroUsize);
-implement!(u8, NonZeroU8);
-implement!(u16, NonZeroU16);
-implement!(u32, NonZeroU32);
-implement!(u64, NonZeroU64);
-implement!(u128, NonZeroU128);
+implement!(usize, NonZeroUsize, PointerUsize);
+implement!(u8, NonZeroU8, PointerU8);
+implement!(u16, NonZeroU16, PointerU16);
+implement!(u32, NonZeroU32, PointerU32);
+implement!(u64, NonZeroU64, PointerU64);
+implement!(u128, NonZeroU128, PointerU128);
